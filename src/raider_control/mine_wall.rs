ccfb1d6e326f4493baac00a4c24c339aeccc0f2c
Rasmus Hansen <hansen13579@gmail.com>
2023-07-08 12:43:59 +0200

    Cleanup

--- src/raider_control/mine_wall.rs
+++ src/raider_control/mine_wall.rs
@@ -11,18 +11,13 @@ pub fn start_mining_wall(
     mut events: EventReader<InteractedWith>,
 ) {
     for event in events.iter() {
-        info!("Checking interact event");
         if let Ok(target) = minable.get(event.entity) {
-            info!("Found minable entity");
             for mut raider in miner.iter_mut() {
-                info!("Queueing mine wall task");
                 event.add_interaction_to_queue(
                     &mut raider,
                     MineWallTask::new(target),
                 );
             }
-        } else {
-            info!("No minable entity found");
         }
     }
 }
