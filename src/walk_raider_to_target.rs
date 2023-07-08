8d88b2b4bd0cc76dd5c5e50a3fbaa94fbfdd089e
Rasmus Hansen <hansen13579@gmail.com>
2023-07-08 11:37:59 +0200

    Cleanup

--- src/walk_raider_to_target.rs
+++ src/walk_raider_to_target.rs
@@ -1,14 +1,13 @@
-use crate::camera_control::{InteractedWith};
+use crate::camera_control::InteractedWith;
 use crate::prelude::*;
 use crate::tasks::move_to_position_task::MoveToPosition;
-
+use bevy_ecs::query::ReadOnlyWorldQuery;
 
 pub struct RaiderControlPlugin;
 
 impl Plugin for RaiderControlPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_system(move_selected_raider_to_target);
+        app.add_system(move_selected_raider_to_target);
     }
 }
 
@@ -21,13 +20,15 @@ pub struct Standable;
 fn move_selected_raider_to_target(
     mut movable: Query<&mut TaskQueue, (With<PlayerMovable>, With<Selected>)>,
     interactable: Query<(), With<Standable>>,
-    mut events: EventReader<InteractedWith>
+    mut events: EventReader<InteractedWith>,
 ) {
     for event in events.iter() {
         if interactable.get(event.entity).is_ok() {
             for mut raider in movable.iter_mut() {
-                info!("Adding task");
-                event.add_interaction_to_queue(&mut raider, MoveToPosition::new(event.interaction.point));
+                event.add_interaction_to_queue(
+                    &mut raider,
+                    MoveToPosition::new(event.interaction.point),
+                );
             }
         }
     }
